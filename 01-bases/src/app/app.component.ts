11a3a20d519565a326ff1fb546e128ef04196121
Manuel Yerbes <mayerga4@gmail.com>
2022-03-10 09:20:30 +0100

    Comenzando el proyecto Angular

---
+++ 01-bases/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+}
+)
+export class AppComponent {
+  titulo = 'Contador App';
+  // public titulo: string = 'Contador App'; ES LO MISMO
+}
